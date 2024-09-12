768b749d9af12cdd766b7288f0515a78e1a10273
kiabq <conway.joseph@pm.me>
2024-09-12 00:32:52 -0400

    add: initial files for quicksort and binary search

---
+++ algos/snf/binary-quicksort.go
@@ -0,0 +1,13 @@
+package snf
+
+// Sort will sort a slice from low to high using the quicksort algorithm.
+func Sort() {
+	// choose a pivot
+	// return if length is one or less. (base case)
+	// otherewise, sort the array.
+}
+
+// Search will find a given number in a slice.
+func Search() {
+
+}
---
+++ algos/snf/binary-quicksort_test.go
@@ -0,0 +1,11 @@
+package snf
+
+import (
+	"testing"
+)
+
+// TestSort does...
+func TestSort(t *testing.T) {}
+
+// TestSearch does...
+func TestSearch(t *testing.T) {}
